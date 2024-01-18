daa0a42b4d881f960fffaafa73586adfe96f9b02
obaibula <baibula.oo@gmail.com>
2024-01-18 17:38:37 +0200

    Optimize imports

--- src/test/java/org/oleh/awsdeploydemo/FailingTest.java
+++ src/test/java/org/oleh/awsdeploydemo/FailingTest.java
@@ -1,8 +1,5 @@
 package org.oleh.awsdeploydemo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FailingTest {
